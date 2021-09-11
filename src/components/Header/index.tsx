17e4c87678f4cb55a08700be3e9497089038a75a
Rodrigo Carvalho <rhrc09@gmail.com>
2021-09-11 09:50:59 -0300

    refactor: update Header props interface

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { Container } from "./styles";
 import Logo from "../../assets/logo.svg";
 
 interface HeaderProps {
-  openModal: () => {};
+  openModal: () => void;
 }
 
 const Header = ({ openModal }: HeaderProps) => {
